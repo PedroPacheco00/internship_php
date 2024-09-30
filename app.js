7e6d588ca28fbe296ccc48e3cd2b5c622f2bce0e
Pedro Pacheco <ppachecol95@gmail.com>
2024-09-30 00:41:05 -0300

    Incluindo HTML e JS adicional para receber a sequencia do usuario

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// Função para retornar a sequência analisada
+function botaoAnalisar() {
+    const sequenciaDigitada = document.querySelector('input').value; // Capturando input
+    const resposta = document.getElementById('resposta'); // Criando variável para a resposta
+    const mensagem = document.getElementById('mensagem'); //Criando variável para mensagem na tela
+   
+    const regex = /^(\d+,)*\d+$/;  // Expressão para validar o formato do input
+    
+    // Checa se o formato do input é válido
+    if (!regex.test(sequenciaDigitada)) {
+      mensagem.innerHTML = 'Formato digitado inválido. Digite uma sequência de números seguindo as orientações abaixo e aperte o botão'; // Envia mensagem de erro
+      return;
+    } else {
+      mensagem.innerHTML = 'Se desejar analisar outra sequência, digite novamente na caixa abaixo e aperte o botão'; // Inclui mensagem para fazer nova análise
+    }
+   
+    // Transformar a string em array de números
+    const sequencia = sequenciaDigitada.split(',');
+   
+    // Chamando o método para contar as ocorrências
+    const { contagem, ordemDaSequencia } = analisadorDeSequencia.contarOcorrencias(sequencia);
+   
+    let respostaFormatada = ""; // Criando variável para armazenar a resposta formatada
+   
+    // Iterando dentro da array ordemDaSequencia para formatar resposta
+    for (let i=0 ; i < ordemDaSequencia.length ; i++) {
+      respostaFormatada +=`${ordemDaSequencia[i]} aparece ${contagem[ordemDaSequencia[i]]} `+(contagem[ordemDaSequencia[i]] == 1 ? "vez" : "vezes")+ "<br>";
+    }
+    
+    resposta.innerHTML = respostaFormatada; // Atribui a resposta formatada ao campo de resposta do HTML
+  }
+  
+
 // Objeto para executar as tarefas
 const analisadorDeSequencia = {
     // Método para contar as ocorrências dos números na sequência
@@ -28,7 +61,7 @@ const analisadorDeSequencia = {
         
         // Iteração para imprimir os resultados no console
         for (let i=0 ; i < ordemDaSequencia.length ; i++) {
-          console.log(`${ordemDaSequencia[i]} ocorre ${contagem[ordemDaSequencia[i]]} vezes`)
+          console.log(`${ordemDaSequencia[i]} ocorre ${contagem[ordemDaSequencia[i]]} `+(contagem[ordemDaSequencia[i]] == 1 ? "vez" : "vezes"))
         }
       }
     };
