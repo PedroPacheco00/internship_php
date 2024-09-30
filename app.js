79c351c078dfd02b7caa0e4dc7999e57cfb7a8cc
Pedro Pacheco <ppachecol95@gmail.com>
2024-09-30 00:35:11 -0300

    Alterando codigo para corrigir ordem da sequencia

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const analisadorDeSequencia = {
     // Método para contar as ocorrências dos números na sequência
     contarOcorrencias: function(sequencia) {
-      const contagem = {};  // Objeto para armazenar as contagens
+      const contagem = {};  // Objeto para armazenar as contagens e a ordem dos numeros
+      const ordemDaSequencia = []; //Array para armazenar a ordem dos numeros que surgem na sequencia
       
       // Iteração para percorrer a sequência
       for (let i = 0; i < sequencia.length; i++) {
@@ -13,23 +14,25 @@ const analisadorDeSequencia = {
           contagem[numero] += 1;  // Incrementa se já estiver no objeto
         } else {
           contagem[numero] = 1;  // Inicializa com 1 se não estiver no objeto
+          ordemDaSequencia.push(numero); // Armazena o novo número da sequência na array
         }
       }
   
-      // Retorna o objeto contagem e a array
-      return {contagem};
+      // Retorna o objeto contagem e a array com a ordem da sequência
+      return {contagem, ordemDaSequencia};
     },
   
     // Método para imprimir os resultados formatados
     imprimirResultados: function(sequencia) {
-      const { contagem } = this.contarOcorrencias(sequencia);  // Obter contagem e ordem da sequencia
-      
-      // Iteração para imprimir os resultados no console
-      for (let numero in contagem) {
-        console.log(`${numero} ocorre ${contagem[numero]} `+(contagem[numero] == 1 ? "vez" : "vezes"));
+        const { contagem, ordemDaSequencia } = this.contarOcorrencias(sequencia);  // Obter contagem e ordem da sequencia
+        
+        // Iteração para imprimir os resultados no console
+        for (let i=0 ; i < ordemDaSequencia.length ; i++) {
+          console.log(`${ordemDaSequencia[i]} ocorre ${contagem[ordemDaSequencia[i]]} vezes`)
+        }
       }
-    }
-  };
+    };
+    
 
 // Exemplo de teste
 const sequencia = [1,2,2,1,4,3,2,1,2,3,1,5];
